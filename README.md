fb5b332147d5f09842897b42116b3c0c0006efdc
gabriel <gabrielbchuede@hacari.org>
2023-09-14 16:42:40 -0300

    funcionalidade argumentos no terminal e documentação README

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Aviso
 Este programa nao possui garantia nenhuma, use por sua conta e risco.
-# descrição
+# Descrição
 Script para python que permite saber a relação nirf-nome de imoveis
 declarados em um ano mas nao em outro. ainda outros dados uteis como
 quantidade de declarações feitas nos anos comparados e total de
@@ -12,14 +12,10 @@ arquivo de texto com os nomes dos contribuintes e os respectivos nirfs
 que foram declarados com o programa itr 2022 mas ainda nao foram
 feitos com o itr 2023, assim fica facil saber quem falta fazer quando
 a quantidade de declarações feitas todo ano passa das centenas.
+```
+python itr-util --ano1 2022 --ano2 2023 --saida saida.txt
+```
 # Parâmetros
-Modifique as variaveis abaixo para respectivamente o caminho dos dados
-do programa itr 2022, programa do itr 2023 e arquivo de texto para a
-saida do relatorio.
-```
-dados2022 = 'C:\\Arquivos de Programas RFB\\ITR2022\\aplicacao\\dados'
-dados2023 = 'C:\\Arquivos de Programas RFB\\ITR2023\\aplicacao\\dados'
-saida = 'C:\\Users\\Vitor\\Documents\\codigo\\saida.txt'
-```
+--ano1, --ano2 e --saida, para, respectivamente, o ano a ser usado como guia, o ano a ser comparado com o ano guia e ser vefificado quanto a declarações nao feitas e o caminho do arquivo de saida (use \\ para windows).
 # Uso
 Apos clonar, use python itr-util no diretorio.
--- itr-util.py
+++ itr-util.py
@@ -2,24 +2,32 @@ import xml.etree.ElementTree as ET
 import os
 import filecmp
 from datetime import date
+import argparse
 
-dados2022 = 'C:\\Arquivos de Programas RFB\\ITR2022\\aplicacao\\dados'
-dados2023 = 'C:\\Arquivos de Programas RFB\\ITR2023\\aplicacao\\dados'
-saida = 'C:\\Users\\Vitor\\Documents\\codigo\\saida.txt'
+parser = argparse.ArgumentParser(description='Mostra quem falta fazer a declaração do ITR.')
+parser.add_argument('--ano1', default='2022', type=int, help='Ano do ITR a ser tomado como guia')
+parser.add_argument('--ano2', default='2023', type=int, help='Ano do ITR a ser analizado')
+parser.add_argument('--saida', default='C:\\Users\\Vitor\\Documents\\saida.txt', help='Caminho do arquivo no qual será escrito o relatório')
+args = parser.parse_args()
+ano1 = args.ano1
+ano2 = args.ano2
+saida = args.saida
+dados1 = f'C:\\Arquivos de Programas RFB\\ITR{ano1}\\aplicacao\\dados'
+dados2 = f'C:\\Arquivos de Programas RFB\\ITR{ano2}\\aplicacao\\dados'
 with open(saida, 'w') as saida:
-    now = date.today()
-    saida.write(f'ITR 2022 feito mas 2023 não feito, em {now.strftime("%d/%m/%y")}\n\n')
-    comparador = filecmp.dircmp(dados2022, dados2023)
+    now = date.today().strftime("%d/%m/%y")
+    saida.write(f'ITR {ano1} feito mas {ano2} não feito, em {now}\n\n')
+    comparador = filecmp.dircmp(dados1, dados2)
     listaDiferença = comparador.left_only
-    total2022 = len(comparador.left_list)
-    total2023 = len(comparador.right_list)
+    total1 = len(comparador.left_list)
+    total2 = len(comparador.right_list)
     total = len(listaDiferença)
-    saida.write(f'quantidade 2022: {total2022}\n')
-    saida.write(f'quantidade 2023: {total2023}\n')
+    saida.write(f'quantidade {ano1}: {total1}\n')
+    saida.write(f'quantidade {ano2}: {total2}\n')
     saida.write(f'quantidade: {total}\n\n')
     saida.write('NIRF          NOME\n')
     for dir in listaDiferença:
-        dec = os.path.join(dados2022, dir) + f'\\{dir}.xml'
+        dec = os.path.join(dados1, dir) + f'\\{dir}.xml'
         tree = ET.parse(dec)
         root = tree.getroot()
         nome = root[0][0].attrib['nomeContribuinte']
